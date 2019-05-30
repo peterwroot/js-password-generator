77b031ecadcce015b6f9cc81e4ccfbcbdbd248d8
Peter Wroot <peter.wroot@infinitymail.co.uk>
2019-05-30 10:09:35 +0100

    design and code changes

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function generatePassword() {
+    /* Sets the text size based on screen width and password length. */
     var passwordLength = document.getElementById("passwordLength").value;
-    var windowSize = window.innerWidth;
-    var newFontSize = Math.floor((windowSize / passwordLength) );
+    var newFontSize = Math.floor((window.innerWidth / passwordLength));
     document.getElementById("generatedPassword").style.fontSize = newFontSize + "pt";
+    /* Sets up the lists of available characters */
     var availableChars_Numeric = "0123456789";
     var availableChars_Special = '!"£$%^&*_+=-@~#';
     var availableChars_Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var availableChars_Lowercase = "abcdefghijklmnopqrstuvwxyz";
     var availableCharacterList="";
+    /* Runs through the checkboxes and determines the available characters */
     if (document.getElementById("specialCheckbox").checked === true){
         availableCharacterList = availableCharacterList.concat(availableChars_Special);
     };
@@ -20,22 +22,22 @@ function generatePassword() {
     if (document.getElementById("lowercaseCheckbox").checked === true){
         availableCharacterList = availableCharacterList.concat(availableChars_Lowercase);
     };
+    /* If the 'readable' checkbox is checked, we remove some ambiguous/hard-to-read characters from the pool */
     if (document.getElementById("readableCheckbox").checked === true){
         availableCharacterList = availableCharacterList.replace(/I|O|L|S|i|o|l|s|-|_|&|"|~|=/g, "");
     };
+    /* Generates the password by picking a random character for the list x times, where x is the password length */
     var generatedPassword = "";
     for (i = 0; i < passwordLength; i++) {
         var getRandomCharacter = availableCharacterList[Math.floor(Math.random() * availableCharacterList.length)];
         generatedPassword += getRandomCharacter;
-    }
+    };
+    /* Only shows the generated password if the user has enabled at least one character checkbox */
     if (availableCharacterList != "") {
         document.getElementById("generatedPassword").innerHTML = generatedPassword;
-    } else {
-        document.getElementById("generatedPassword").innerHTML = "Nice try."
-    }
-    document.getElementById("generatedPassword").style.opacity = 1;
-    document.getElementById("copyButton").style.opacity = 1;
+    };
 }
+/* Copies the generated password to the user's clipboard. */
 function copyToClipboard() {
     document.getElementById("toCopy").value=document.getElementById("generatedPassword").innerHTML;
     var copyText = document.getElementById("toCopy");
