85a3fde51c56c247581c37571c795e925aa2632c
Peter Wroot <peter.wroot@infinitymail.co.uk>
2019-06-20 12:24:19 +0100

    re-design with bootstrap

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 function generatePassword() {
     /* Sets the text size based on screen width and password length. */
     var passwordLength = document.getElementById("passwordLength").value;
-    
+    var newFontSize = Math.floor((window.innerWidth / (passwordLength * 2)));
+    document.getElementById("generatedPassword").style.fontSize = newFontSize + "pt";
     /* Sets up the lists of available characters */
     var availableChars_Numeric = "0123456789";
     var availableChars_Special = '!"£$%^&*_+=-@~#';
     var availableChars_Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var availableChars_Lowercase = "abcdefghijklmnopqrstuvwxyz";
-    var availableCharacterList="";
+    var availableCharacterList = "";
     /* Runs through the checkboxes and determines the available characters */
-    if (document.getElementById("specialCheckbox").checked === true){
+    if (document.getElementById("specialCheckbox").checked === true) {
         availableCharacterList = availableCharacterList.concat(availableChars_Special);
     };
-    if (document.getElementById("numberCheckbox").checked === true){
+    if (document.getElementById("numberCheckbox").checked === true) {
         availableCharacterList = availableCharacterList.concat(availableChars_Numeric);
     };
-    if (document.getElementById("uppercaseCheckbox").checked === true){
+    if (document.getElementById("uppercaseCheckbox").checked === true) {
         availableCharacterList = availableCharacterList.concat(availableChars_Uppercase);
     };
-    if (document.getElementById("lowercaseCheckbox").checked === true){
+    if (document.getElementById("lowercaseCheckbox").checked === true) {
         availableCharacterList = availableCharacterList.concat(availableChars_Lowercase);
     };
     console.log(availableCharacterList)
     /* If the 'readable' checkbox is checked, we remove some ambiguous/hard-to-read characters from the pool */
-    if (document.getElementById("readableCheckbox").checked === true){
+    if (document.getElementById("readableCheckbox").checked === true) {
         availableCharacterList = availableCharacterList.replace(/I|O|L|S|i|o|l|s|-|_|&|"|~|=/g, "");
     };
     /* Generates the password by picking a random character for the list x times, where x is the password length */
@@ -39,7 +40,7 @@ function generatePassword() {
 }
 /* Copies the generated password to the user's clipboard. */
 function copyToClipboard() {
-    document.getElementById("toCopy").value=document.getElementById("generatedPassword").innerHTML;
+    document.getElementById("toCopy").value = document.getElementById("generatedPassword").innerHTML;
     var copyText = document.getElementById("toCopy");
     copyText.select();
     document.execCommand("copy");
